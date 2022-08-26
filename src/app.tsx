3b8efea2b202fde1469b404c6e9cc5ccc8bc0173
Diego Marcelino <49411570+diegomarcelino92@users.noreply.github.com>
2022-08-26 16:07:18 -0300

    Feat/issue 2 (#3)
    * feat: adding component a
    
    * feat: adding component a

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { ComponentA } from './component-a'
 
 export const App: React.FC = () => (
   <div>
     <h1>Trunk Based Development</h1>
+    <ComponentA />
   </div>
 )
