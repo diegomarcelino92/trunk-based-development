646509c4d702e155945a19ad836d58c987dff393
diegomarcelino92 <ddmarcelino2014@gmail.com>
2022-08-26 13:55:07 -0300

    feat: remove app2

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,7 @@
 import React from 'react'
 
-export const App: React.FC = () => <div>APP</div>
+export const App: React.FC = () => (
+  <div>
+    <h1>Trunk Based Development</h1>
+  </div>
+)
--- src/app2.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-export const App2: React.FC = () => <div>APP - 2</div>
