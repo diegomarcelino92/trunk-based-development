6cfdf451e55dff84c6d414ad7054146ec7e7927c
diegomarcelino92 <ddmarcelino2014@gmail.com>
2022-08-26 16:14:34 -0300

    feat:  adding component b

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { ComponentA } from './component-a'
 
+const FeatureFlagProvider = {
+  issue: false
+}
+
 export const App: React.FC = () => (
   <div>
     <h1>Trunk Based Development</h1>
-    <ComponentA />
+    {FeatureFlagProvider.issue && <ComponentA />}
   </div>
 )
